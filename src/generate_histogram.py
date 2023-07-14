1da5eb2cd1d10ac8894619939b866532e79f0093
LonFerchoSR <lsanchezr1500@alumno.ipn.mx>
2023-07-13 23:41:45 -0600

    update

--- src/generate_histogram.py
+++ src/generate_histogram.py
@@ -16,7 +16,12 @@ def plot_histogram(df, column, output_path):
 
 
 def main():
+    # Se crea variable reader, que se vuelve una clase al llamar a Dataloader (se incluye el path)
+    # ya que es necesario para saber de donde tomar la información
+
     reader = Dtl.Dataloader("./data/processed/RH_procesado.csv")
+
+    # df es dataframe, se crea con la funcion de load_data
 
     df = reader.load_data()
 
